b93ed6baca1bf496f5b93b73ff95417605235282
JordiTi <Jordi.t96i@gmail.com>
2020-09-30 23:15:02 +0200

    Traffic lights now know how many cars they get from who

--- TrafficLight.py
+++ TrafficLight.py
@@ -1,12 +1,48 @@
 from mesa import Agent, Model
 import random
+import numpy as np
+
 
 class TrafficLightAgent(Agent):
-    def __init__(self, name, model,trafficColor):
-        super().__init__(name, model)
-        self.trafficColor = "red"
+    def __init__(self, name, intersectionmodel, trafficColor, direction, number):
+        super().__init__(name, intersectionmodel)
+        self.model = intersectionmodel
+        self.id = number
+        self.trafficColor = trafficColor
+        self.direction = direction
 
     def step(self):
+        infront = 0
+        if self.direction == "N":
+            for i in range(int(self.model.streetlength / self.model.gridsize)):
+                if not self.model.grid.out_of_bounds((self.pos[0], self.pos[1] - i)):
+                    gridcoordinates = (self.pos[0], self.pos[1] - i)
+                    agents = self.model.grid.get_cell_list_contents(gridcoordinates)
+                    infront = infront + len(agents)
+
+        if self.direction == "E":
+            for i in range(int(self.model.streetlength / self.model.gridsize)):
+                if not self.model.grid.out_of_bounds((self.pos[0] - i, self.pos[1])):
+                    gridcoordinates = (self.pos[0] - i, self.pos[1])
+                    agents = self.model.grid.get_cell_list_contents(gridcoordinates)
+                    infront = infront + len(agents)
+
+        if self.direction == "S":
+            for i in range(int(self.model.streetlength / self.model.gridsize)):
+                if not self.model.grid.out_of_bounds((self.pos[0], self.pos[1] + i)):
+                    gridcoordinates = (self.pos[0], self.pos[1] + i)
+                    agents = self.model.grid.get_cell_list_contents(gridcoordinates)
+                    infront = infront + len(agents)
+        if self.direction == "W":
+            for i in range(int(self.model.streetlength / self.model.gridsize)):
+                if not self.model.grid.out_of_bounds((self.pos[0] + i, self.pos[1])):
+                    gridcoordinates = (self.pos[0] + i, self.pos[1])
+                    agents = self.model.grid.get_cell_list_contents(gridcoordinates)
+                    infront = infront + len(agents)
+        np.set_printoptions(threshold=np.inf)
+        infront = infront - 1 # traffic light counts itself
+        self.model.tlightmatrix[self.id, :][self.model.tlightmatrix[self.id, :] >= 0] = infront/3
+        print(self.model.tlightmatrix)
         if random.randint(0, 100) < 10:
             if self.trafficColor == "red":
                 self.trafficColor = "green"
